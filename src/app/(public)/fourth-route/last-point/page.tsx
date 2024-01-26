52ae1864d8037d8b0f3cfb08173c832286d92d24
tyzrex <sulavbaral58@gmail.com>
2024-01-26 23:45:32 +0545

    new routes

---
+++ src/app/(public)/fourth-route/last-point/page.tsx
@@ -0,0 +1,34 @@
+import LastHintPage from "@/components/last-page";
+import { db } from "@/lib/db";
+import { RouteType } from "@/lib/utils";
+
+export const dynamic = "force-dynamic";
+
+export default async function LastPoint() {
+  const maxRoute3Count = await db.scavenger.findFirst({
+    where: {
+      type: "Route3",
+    },
+    orderBy: {
+      id: "desc",
+    },
+  });
+  const lastHint = await db.scavenger.findUnique({
+    where: {
+      id: maxRoute3Count?.id,
+    },
+  });
+
+  if (!lastHint) {
+    return (
+      <div className="text-white text-center">
+        <p className="text-xl font-semibold">No hints found</p>
+      </div>
+    );
+  }
+  return (
+    <>
+      <LastHintPage id={lastHint?.id} route={RouteType.Route3} />
+    </>
+  );
+}
