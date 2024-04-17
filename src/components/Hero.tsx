a02769679315c94784a3c49f4fd98b3e55563a18
tyzrex <sulavbaral58@gmail.com>
2024-04-17 20:08:05 +0545

    Add TEDx logo to Hero component and display it

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,10 @@
+import TedLogo from "../../public/ted.webp";
+
 export default function ScavengerHero() {
   return (
     <>
       <div className="flex flex-col items-start justify-center mt-10 gap-5 px-3">
-        <h1 className="text-2xl md:text-6xl font-bold text-left">
-          <span className="text-[#E62B1E]">
-            TED<sup className="text-4xl">x</sup>
-          </span>
-          <span className="ml-5 font-normal text-white">Maitighar</span>
-        </h1>
+        <img src={TedLogo.src} alt="TEDxMaitighar" className="w-50 md:w-100" />
       </div>
     </>
   );
